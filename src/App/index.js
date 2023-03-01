75c113df99fe95099b98d871ed822bec90b47aa1
Babak <jahanibabak80@gmail.com>
2023-03-02 01:00:41 +0330

    productList fix folder structuring

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Product from "../Components/Product";
+import ProductList from "../Components/ProductList";
+
+const App = () => {
+  return (
+    <div>
+      <div>salam</div>
+      <ProductList />
+      <Product />
+    </div>
+  );
+};
+
+export default App;
